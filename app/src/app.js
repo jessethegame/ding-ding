cccea9531a35978794188228ee69ba5d8031d3a3
Tak Tran <contact@tutaktran.com>
2014-10-31 16:08:29 +0000

    Hook up keyboard for web interface.

--- app/src/app.js
+++ app/src/app.js
@@ -3,6 +3,13 @@
   var socketIOUrl = 'http://localhost:7771';
   var socket = io(socketIOUrl);
 
+  var keyServoIndex = {
+    97: 0, // 'a',
+    115: 1, // 's',
+    100: 2, // 'd',
+    102: 3 // 'f'
+  }
+
   function clickButton(index) {
     console.log('emit', index);
     socket.emit('button', { index: index });
@@ -17,5 +24,14 @@
 
   socket.on('connect', function (data) {
     console.log("Connected to", socketIOUrl);
+  });
+
+  $(document).keypress(function(event) {
+    console.log( "Key", event.which);
+
+    var servoIndex = keyServoIndex[event.which];
+    if (_.isNumber(servoIndex)) {
+      clickButton(servoIndex);
+    }
   });
 })();

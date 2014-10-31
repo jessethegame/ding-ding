22690d06462b7bf76d6a36b0629c32d0bf3f18a9
Tak Tran <contact@tutaktran.com>
2014-10-31 11:58:53 +0000

    Support 4 servos.

--- bin/ding-ding/index.js
+++ bin/ding-ding/index.js
@@ -4,6 +4,7 @@
 "use strict";
 
 var keypress = require('keypress');
+var _ = require('lodash');
 
 var five = require("johnny-five");
 var board = new five.Board();
@@ -11,9 +12,8 @@ var board = new five.Board();
 // Set up of the board
 var pins = {
   pullUpButton: 2,  // Other pin in GND
-  led: 13,          // Negative end in GND
-  servo: 10,
-  motor: 5          // Other pin in GND
+  led: 0,          // Negative end in GND
+  motor: 5,          // Other pin in GND
 };
 
 var constants = {
@@ -21,7 +21,24 @@ var constants = {
   ledOnOffSpeed: 50,
 
   servoMin: 135,
-  servoMax: 180
+  servoMax: 180,
+
+  // Also defines how many servos there are, and
+  // what index they are
+  servos: [
+    {
+      pin: 6
+    },
+    {
+      pin: 11
+    },
+    {
+      pin: 10
+    },
+    {
+      pin: 9
+    }
+  ]
 }
 
 board.on("ready", function() {
@@ -40,15 +57,22 @@ board.on("ready", function() {
 
   var led = new five.Led(pins.led);
 
-  var servo = new five.Servo({
-    pin: pins.servo,
-    // Ding goes from min to max
-    range: [constants.servoMin, constants.servoMax],
-    startAt: constants.servoMin
+  var servos = [];
+
+  _.each(constants.servos,function(servoConfig, index) {
+    servos[index] = new five.Servo({
+      pin: servoConfig.pin,
+      // Ding goes from min to max
+      range: [constants.servoMin, constants.servoMax],
+      startAt: constants.servoMin,
+      isInverted: true
+    });
   });
 
+
+
   board.repl.inject({
-    servo: servo,
+    servos: servos,
     button: button,
     led: led
   });
@@ -61,11 +85,21 @@ board.on("ready", function() {
     led.off();
   });
 
-  function swingServo() {
-    servo.max();
-    setTimeout(function() {
-      servo.min();
-    }, constants.servoSwingSpeed);
+  /**
+   * Generate a function to swing the servo
+   *
+   * @param  {integer} servoIndex
+   * @return {function} Function to swing the servo
+   */
+  function swingServo(servoIndex) {
+    return function() {
+      var servo = servos[servoIndex];
+
+      servo.max();
+      setTimeout(function() {
+        servo.min();
+      }, constants.servoSwingSpeed);
+    };
   }
 
   function ledOnOff() {
@@ -76,7 +110,10 @@ board.on("ready", function() {
   }
 
   var keymap = {
-    a: swingServo
+    a: swingServo(0),
+    s: swingServo(1),
+    d: swingServo(2),
+    f: swingServo(3),
   };
 
   process.stdin.on("keypress", function(ch, key) {

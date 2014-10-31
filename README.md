f9d8e12cdae05a2976614e7a486e80ef85838667
Tak Tran <contact@tutaktran.com>
2014-10-31 11:02:29 +0000

    Add key press to ding ding.

--- README.md
+++ README.md
@@ -8,72 +8,14 @@ See [`bin`](https://github.com/taktran/starttter-hardware/tree/master/bin) folde
 
 ## Development
 
+Wire everything up (check the file for pins).
+
 Install pre-requisites
 
     npm install
 
-### Spark core
+Run the app
 
-Pre-requisites
+    node bin/ding-ding
 
-* [Spark core](https://www.spark.io/)
-* `npm install -g spark-cli`
-* `spark cloud login`
-* Copy `app/public/config/config.example.js` to `app/public/config/config.js`, and add in spark core id
-
-Start the server
-
-* `npm install -g nodemon`
-
-Get device state
-
-    spark get [device_name] state
-
-Monitor state
-
-    spark monitor [device_name] state 5000
-
-Update color
-
-    spark call [device_name] update "100;100;0"
-
-To flash the device with code
-
-    spark flash [device_name] bin/spark/rgb-light.ino
-
-#### Serial
-
-To debug the spark, you can monitor serial output.
-
-Firstly, in the setup code
-
-    void setup() {
-        // Start serial connection
-        Serial.begin(9600);
-
-        // ...
-    }
-
-List connected serial devices
-
-    spark serial list
-
-Connect to device
-
-    spark serial monitor [list_id]
-
-## Testing
-
-Uses [karma](http://karma-runner.github.io/) and [jasmine](http://pivotal.github.io/jasmine/).
-
-Karma is run automatically when `grunt` is called. To run it manually
-
-    karma start config/karma.conf.js
-
-For continuous integration, run
-
-    grunt ci:test
-
-    # Or,
-
-    npm test
+To close the app, ctrl-c a couple of times.
--- bin/ding-ding/index.js
+++ bin/ding-ding/index.js
@@ -3,6 +3,8 @@
  */
 "use strict";
 
+var keypress = require('keypress');
+
 var five = require("johnny-five");
 var board = new five.Board();
 
@@ -10,10 +12,26 @@ var board = new five.Board();
 var pins = {
   pullUpButton: 2,  // Other pin in GND
   led: 13,          // Negative end in GND
+  servo: 10,
   motor: 5          // Other pin in GND
 };
 
+var constants = {
+  servoSwingSpeed: 150,
+  ledOnOffSpeed: 50,
+
+  servoMin: 135,
+  servoMax: 180
+}
+
 board.on("ready", function() {
+
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+
+  if (typeof process.stdin.setRawMode === 'function') {
+    process.stdin.setRawMode(true);
+  } // Else not needed?
 
   var button = new five.Button({
     pin: pins.pullUpButton,
@@ -22,35 +40,55 @@ board.on("ready", function() {
 
   var led = new five.Led(pins.led);
 
-  // Create a new `motor` hardware instance.
-  var motor = new five.Motor({
-    pin: pins.motor
+  var servo = new five.Servo({
+    pin: pins.servo,
+    // Ding goes from min to max
+    range: [constants.servoMin, constants.servoMax],
+    startAt: constants.servoMin
   });
 
   board.repl.inject({
-    motor: motor,
+    servo: servo,
     button: button,
     led: led
   });
 
   button.on("down", function(value) {
     led.on();
-    motor.start();
   });
 
   button.on("up", function() {
     led.off();
-    motor.stop();
   });
 
-  // Motor Event API
-  motor.on("start", function(err, timestamp) {
-    // motor.isOn === true
-    console.log("start", timestamp);
-  });
+  function swingServo() {
+    servo.max();
+    setTimeout(function() {
+      servo.min();
+    }, constants.servoSwingSpeed);
+  }
 
-  motor.on("stop", function(err, timestamp) {
-    // motor.isOn === false
-    console.log("stop", timestamp);
+  function ledOnOff() {
+    led.on();
+    setTimeout(function() {
+      led.off();
+    }, constants.ledOnOffSpeed);
+  }
+
+  var keymap = {
+    a: swingServo
+  };
+
+  process.stdin.on("keypress", function(ch, key) {
+    if (!key) {
+      return;
+    }
+
+    var keyMapFunc = keymap[key.name];
+    if (keyMapFunc) {
+      keyMapFunc();
+      ledOnOff();
+    }
+
   });
 });

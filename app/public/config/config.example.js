b46701cfcd10b09a232091e9c4d76932697cee67
Tak Tran <contact@tutaktran.com>
2014-10-31 14:33:46 +0000

    Init front end.

--- app/public/config/config.example.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  "use strict";
-  var CONFIG = {
-    sparkCoreId: "some_id",
-    accessToken: "some_access_token"
-  };
-
-  window.CONFIG = CONFIG;
-})();
--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,41 +1,15 @@
 (function() {
   "use strict";
 
-  var urlPrefix = "https://api.spark.io/v1/devices/" + CONFIG.sparkCoreId;
-  var accessToken = "access_token=" + CONFIG.accessToken;
-  var stateUrl = urlPrefix + "/state?" + accessToken;
-  var sparkEventsUrl = "https://api.spark.io/v1/events?" + accessToken;
-
-  function setCoreColour(colorObj) {
-    var rgbStr = colorObj.r + "," + colorObj.g + "," + colorObj.b;
-    $(".rgb-color").css("background-color", "rgb(" + rgbStr + ")");
+  function clickButton(index) {
+    console.log("click", index);
   }
 
-  // Get initial colour
-  $.get(stateUrl)
-    .done(function(data) {
-      var result = data["result"];
-
-      try {
-        var colorObj = JSON.parse(result);
-        setCoreColour(colorObj);
-      } catch(err) {
-        console.log("Parse error", result, err);
-      }
+  _.each(_.range(4), function(index) {
+    var elemSel = '.servo-' + index;
+    $(elemSel).click(function() {
+      clickButton(index);
     })
-    .fail(function(err) {
-      console.log("Get state error", err);
-    });
-
-  // Subscribe to change events
-  var sparkEvents = new EventSource(sparkEventsUrl);
-  sparkEvents.onmessage = function(e) {
-    console.log(e);
-  };
-
-  sparkEvents.onerror = function(e) {
-    console.log("sparkEvents failed", e);
-  };
-
+  })
 })();
 //# sourceMappingURL=app.js.map
